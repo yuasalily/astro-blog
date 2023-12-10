945c7c844a2f7baf7e0d2831b80249544a487ca6
yuasalily <yuasalily1011@gmail.com>
2023-12-10 14:52:38 +0000

    tailwindのベース作成

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				mycolor: {
+					100: "#ababab",
+					300: "#cdcdcd",
+					500: "#efefef"
+				},
+				myblack: "#0909090"
+			}
+		},
+		fontFamily: {
+			hachimaru:['Hachi Maru Pop']
+
+		}
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
