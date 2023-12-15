ba44c3d1cee809c9a08b3bfcf39ff65b09b106b2
yuasalily <yuasalily1011@gmail.com>
2023-12-15 16:54:51 +0000

    biome導入

--- src/samples/react/Example.jsx
+++ src/samples/react/Example.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 const Example = () => {
-    const[ count, setCount ] = useState(0);
+  const [count, setCount] = useState(0);
+  const a = 1;
 
-    return(
-      <>
-        <h1 className="text-red-700">Counter App</h1>
-        <h2 className="text-blue-700">{count}</h2>
-        <button onClick={()=> setCount(count+1)}>+ number1</button>
-      </>
-    );
+
+  return (
+    <>
+      <h1 className="text-red-700">Counter App</h1>
+      <h2 className="text-blue-700">{count}</h2>
+      <button type="button" onClick={() => setCount(count + 1)}>+ number1</button>
+    </>
+  );
 };
 
 export default Example;
