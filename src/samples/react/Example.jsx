64584a0424d0bbc6185102cf65653fbcce0b173f
yuasalily <yuasalily1011@gmail.com>
2023-12-15 16:22:38 +0000

    react導入

---
+++ src/samples/react/Example.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+const Example = () => {
+    const[ count, setCount ] = useState(0);
+
+    return(
+      <>
+        <h1 className="text-red-700">Counter App</h1>
+        <h2 className="text-blue-700">{count}</h2>
+        <button onClick={()=> setCount(count+1)}>+ number1</button>
+      </>
+    );
+};
+
+export default Example;
