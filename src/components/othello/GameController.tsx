2878a7fb98f7a87a4c7dba6c248aa8bcd5c0f337
yuasalily <yuasalily1011@gmail.com>
2023-12-28 07:43:11 +0000

    リセットボタン追加

---
+++ src/components/othello/GameController.tsx
@@ -0,0 +1,47 @@
+import {
+	useHamburgerAssetDispatch,
+	useHamburgerBoardDispatch,
+	useHamburgerRevenueDispatch,
+	usePizzaAssetDispatch,
+	usePizzaBoardDispatch,
+	usePizzaRevenueDispatch,
+	useSetEventText,
+	useSetExpectedEventText,
+	useTurnDispatch,
+} from "@components/othello/context/BoardContext";
+
+const GameController = () => {
+	const pizzaBoardDispatch = usePizzaBoardDispatch();
+	const pizzaRevenueDispatch = usePizzaRevenueDispatch();
+	const pizzaAssetDispatch = usePizzaAssetDispatch();
+
+	const hamburgerBoardDispatch = useHamburgerBoardDispatch();
+	const hamburgerRevenueDispatch = useHamburgerRevenueDispatch();
+	const hamburgerAssetDispatch = useHamburgerAssetDispatch();
+
+	const turnDispatch = useTurnDispatch();
+
+	const setEventText = useSetEventText();
+	const setExpectedEventText = useSetExpectedEventText();
+
+	const handleResetClick = () => {
+		pizzaBoardDispatch({ type: "initialize", payload: 0n });
+		pizzaRevenueDispatch({ type: "initialize", payload: 0 });
+		pizzaAssetDispatch({ type: "initialize", payload: 0 });
+
+		hamburgerBoardDispatch({ type: "initialize", payload: 0n });
+		hamburgerRevenueDispatch({ type: "initialize", payload: 0 });
+		hamburgerAssetDispatch({ type: "initialize", payload: 0 });
+
+		turnDispatch("initialize");
+
+		setEventText("");
+		setExpectedEventText("");
+	};
+	return (
+		<button type="button" onClick={handleResetClick}>
+			リセットする
+		</button>
+	);
+};
+export default GameController;
